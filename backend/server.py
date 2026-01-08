9cfbf03a4197fe002831ac589832e027d5979357
dkatunic21 <dkatunic21@foi.hr>
2026-01-08 20:17:22 +0100

    Added jwt token authentication

--- backend/server.py
+++ backend/server.py
@@ -1,38 +1,49 @@
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, Boolean, ForeignKey, Float, DateTime, and_
 from functools import wraps
+from flask_jwt_extended import jwt_required
+from datetime import datetime, timedelta
+import jwt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fjm034jhf0439uf423huj546890z2mf03j406h4v'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///online_shop.db"
+app.config["JWT_SECRET_KEY"] = "hnf9weh809f208h9453207tgzh0f2bg208hj9321hrt"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+def jwt_required(role=None):
+    def wrapper(function):
+        @wraps(function)
+        def check_jwt(*args, **kwargs):
+            token = request.cookies.get('access_token')
+            if not token:
+                return abort(403)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_or_404(User, user_id)
+            try:
+                payload = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=["HS256"])
+            except jwt.ExpiredSignatureError:
+                return abort(401)
+            except jwt.InvalidTokenError:
+                return abort(401)
 
-def admin_only(function):
-    @wraps(function)
-    def check_admin(*args, **kwargs):
-        if current_user.role == 'ADMIN':
+            if role and payload.get('role') != role:
+                return abort(401)
+            
+            request.user = payload
             return function(*args, **kwargs)
-        return abort(403)
-    
-    return check_admin
+        return check_jwt
+    return wrapper
 
-def voter_only(function):
-    @wraps(function)
-    def check_voter(*args, **kwargs):
-        if current_user.role == 'VOTER':
-            return function(*args, **kwargs)
-        return abort(403)
-    
-    return check_voter
+def create_jwt(user):
+    token = jwt.encode(
+        {'user_id': user.id, 'email': user.email, 'role': user.role, 'exp': datetime.utcnow() + app.config['JWT_ACCESS_TOKEN_EXPIRES']},
+        app.config['JWT_SECRET_KEY'],
+        algorithm="HS256"
+    )
+
+    return token
 
 class Base(DeclarativeBase):
     pass
@@ -40,7 +51,7 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-class User(UserMixin, Base):
+class User(Base):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(300), nullable=False)
@@ -70,10 +81,16 @@ def get_post_login():
         user = db.session.execute(db.select(User).where(User.email == email)).scalar()
 
         if password == user.password:
-            login_user(user)
             if user.role == 'ADMIN':
-                return redirect(url_for('get_show_results'))    
-            return redirect(url_for('get_post_vote'))
+                response = make_response(redirect(url_for('get_show_results')))
+                jwt_token = create_jwt(user)
+                response.set_cookie("access_token", jwt_token, httponly=True, secure=False)
+                return response
+            
+            response = make_response(redirect(url_for('get_post_vote')))
+            jwt_token = create_jwt(user)
+            response.set_cookie("access_token", jwt_token, httponly=True, secure=False)
+            return response
     
     return render_template('login.html')
 
@@ -106,15 +123,16 @@ def get_post_register():
         db.session.add(new_User)
         db.session.commit()
 
-        login_user(new_User)
+        response = make_response(redirect(url_for('get_post_vote')))
+        jwt_token = create_jwt(new_User)
+        response.set_cookie("access_token", jwt_token, httponly=True, secure=False)
 
-        return redirect(url_for('get_post_vote'))
+        return response
 
     return render_template('register.html')
 
 @app.route('/vote', methods=["GET", "POST"])
-@login_required
-@voter_only
+@jwt_required('VOTER')
 def get_post_vote():
     if request.method == "POST":
         pass
@@ -122,16 +140,16 @@ def get_post_vote():
     return render_template('vote.html')
 
 @app.route('/results')
-@login_required
-@admin_only
+@jwt_required('ADMIN')
 def get_show_results():
     return "ADMIN ONLY !"
 
 @app.route('/logout')
-@login_required
+@jwt_required()
 def logout():
-    logout_user()
-    return redirect(url_for('get_home_page'))
+    response = make_response(redirect(url_for('get_home_page')))
+    response.delete_cookie('access_token')
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
